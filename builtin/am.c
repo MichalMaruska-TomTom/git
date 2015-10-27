3b04fe05bef5a0c3301cbd08612787b33d18d4f1
Michal Maruska <mmaruska@gmail.com>
2015-10-27 23:31:05 +0100

    Use color conditionally

--- builtin/am.c
+++ builtin/am.c
@@ -1867,7 +1867,7 @@ static void am_run(struct am_state *state, int resume)
 #define install_color "\x1b[33;01m"
 #define remove_color "\x1b[38;5;160m"
 
-                {
+                if (getenv("GIT_USE_COLOR") != NULL) {
                         const char *fmt = _("Applying: %.*s");
                         char *color_fmt;
                         int len = strlen(fmt);
@@ -1878,8 +1878,9 @@ static void am_run(struct am_state *state, int resume)
 
                         say(state, stdout, color_fmt, linelen(state->msg), state->msg);
                         free(color_fmt);
+                } else {
+                        say(state, stdout, _("Applying: %.*s"), linelen(state->msg), state->msg);
                 }
-
 		apply_status = run_apply(state, NULL);
 
 		if (apply_status && state->threeway) {
@@ -1950,7 +1951,7 @@ static void am_resolve(struct am_state *state)
 {
 	validate_resume_state(state);
 
-        {
+        if (getenv("GIT_USE_COLOR") != NULL) {
                 const char *fmt = _("Applying: %.*s");
                 char *color_fmt;
                 int len = strlen(fmt);
@@ -1963,6 +1964,8 @@ static void am_resolve(struct am_state *state)
 
                 say(state, stdout, color_fmt, linelen(state->msg), state->msg);
                 free(color_fmt);
+        } else {
+                say(state, stdout, _("Applying: %.*s"), linelen(state->msg), state->msg);
         }
 
 	if (!index_has_changes(NULL)) {
